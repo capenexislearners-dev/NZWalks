fdc6c403d29c68ed0b853dbaae3bbc703432ab15
proliner <proliner@DESKTOP-QSJ92T2>
2026-02-26 10:38:22 +0200

    we have updated the regions controller and added a RegionRequest DTO

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -4,6 +4,7 @@ using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTOs;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 
 namespace NZWalks.API.Controllers
 {
@@ -11,12 +12,12 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
-        private readonly NZWalksDbContext _context;   
+        private readonly NZWalksDbContext _context;
 
         public RegionsController(NZWalksDbContext context)
         {
             _context = context;
-                
+
         }
         [HttpGet]
 
@@ -40,29 +41,64 @@ namespace NZWalks.API.Controllers
             }
             return Ok(regionsDto);
 
-             
+
         }
 
         [HttpGet]
         [Route("{id:Guid}")]
-        public IActionResult GetById([FromRoute]Guid id)
+        public IActionResult GetById([FromRoute] Guid id)
         {
+            var region = _context.Regions.Find(id);
+
 
             // var region = _context.Regions.Find(id);
-            var region = _context.Regions.FirstOrDefault(x=>x.Id==id);
+            var Region = _context.Regions.FirstOrDefault(x => x.Id == id);
 
 
             if (region == null)
             {
                 return NotFound();
+
+                var regionDto = new RegionDto
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                };
+
             }
             return Ok(region);
-               
+
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody]AddRegionRequestDto addRegionRequestDto)
+        {
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+
+            };
+            _context.Regions.Add(regionDomainModel);
+            _context.SaveChanges();
+
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+
         }
 
-
-
-
+       
 
     }
+
 }
